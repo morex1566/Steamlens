2b6381250616809badd80eb915a51425dd1ac032
Jiwon <morex1566@gmail.com>
2025-08-27 20:20:30 +0900

    깃허브 링크 & 검색 UI 추가

---
+++ docs/steamapi.js
@@ -0,0 +1,20 @@
+// 제목 검색을 할 때 이텔릭에서 기본 폰트로 설정하는 함수
+function changeFont() 
+{
+    const searchBox = document.getElementById('searchInput');
+
+    if (searchBox && searchBox.value.length > 0) 
+    {
+        searchBox.style.fontStyle = 'normal';
+    }
+    else
+    {
+        searchBox.style.fontStyle = 'italic';
+    }
+}
+
+function linkGithub()
+{
+    const address = "https://github.com/morex1566/PlayVoX"
+    window.open(address, "_blank");
+}
